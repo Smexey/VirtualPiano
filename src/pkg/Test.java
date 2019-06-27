d09ddfad01ebed5eac659eb68245430b78b895d2
Pavle Divovic <44965371+pajser7256@users.noreply.github.com>
2019-06-27 12:57:54 +0200

    gittest2

--- src/pkg/Test.java
+++ src/pkg/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("asff");
         System.out.println("GIT COMEEEEET");
+        System.out.println("GIT COMEEEEET2");
     }
 }
